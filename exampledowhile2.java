a01650dd969c8c88cc69d035c79072a522d32f5e
Abhishek Khomane <122467926+XEABHI@users.noreply.github.com>
2024-03-05 04:56:57 -0800

    Add files via upload

---
+++ exampledowhile2.java
@@ -0,0 +1,18 @@
+public class exampledowhile2 {
+    
+
+    public static void main(String[] args) {
+        int a=3;
+
+        do
+        {
+            a+=3;
+            if (a%2==0)
+            {
+                System.out.println(a); 
+
+            }
+        
+        }while(a<=30);
+    }
+}
